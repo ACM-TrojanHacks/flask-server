499e29957b610e306b02da4a1393cd468dd5be5d
JOY BARNES <joyterencebarnes@gmail.com>
2021-11-13 20:52:59 -0800

    Add restful endpoint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,75 +1,31 @@
 import os
 from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
+import werkzeug
+from flask_restful import reqparse, Api, Resource
 
 from .clarifai import detect
 
 UPLOAD_FOLDER = "../uploads"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+app = Flask(__name__)
+api = Api(app)
 
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+class UploadImage(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    def post(self):
+        self.parser.add_argument("file", type=werkzeug.datastructures.FileStorage, location='files')
+        args = self.parser.parse_args()
+        image = args.get("file")
+        filename = os.path.join(app.root_path, UPLOAD_FOLDER, image.filename)
+        image.save(filename)
+        detect(filename)
+        return { 'msg': "Successfull" }, 201
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    def allowed_file(filename):
-        return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-    @app.route('/', methods=['GET', 'POST'])
-    def upload_file():
-        print(request)
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # If the user does not select a file, the browser submits an
-            # empty file without a filename.
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                abs_filename = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename)
-                file.save(abs_filename)
-                detect(abs_filename)
-                return "file_uploaded_successfully"
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-        <input type=file name=file>
-        <input type=submit value=Upload>
-        </form>
-        '''
-
-    return app
+api.add_resource(UploadImage, '/upload_image')
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc')
+    app.run(debug=True, port=5000, host="0.0.0.0", ssl_context='adhoc')
